70efc2c19e22ee1ff61d4120d90fec34f7b979fe
Michal.Jíra <michaljira24@gmail.com>
2025-03-29 20:20:08 +0100

    shadcn{ui dowload and reinstalation of client and tailwindcss instalation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import AppRoutes from './pages/AppRoutes'
+import { Button } from './components/ui/button'
 
 function App() {
+  const [count, setCount] = useState(0)
 
-  return(
+  return (
     <>
-    <AppRoutes/>
+    <Button>
+    </Button>
     </>
-  );
+  )
 }
-
 
 export default App
