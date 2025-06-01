7998e298dea04a3efa9424bba6d3bb8ac98379c7
Jan Šípek <jansipek12@gmail.com>
2025-06-01 23:42:24 +0200

    update

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -1,4 +1,4 @@
-const Recipe = require('../models/recipe'); // uprav cestu podle názvu souboru/modelu
+const Recipe = require('../models/recipe');
 
 exports.getAllRecipes = async (req, res) => {
   try {
